e556e914ca3213ede2524caf66c95546a6511468
aenuros <37673253+aenuros@users.noreply.github.com>
2023-11-02 13:58:59 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+<html>
+    <head>
+        <title>test</title>
+
+        <style>
+            .over {
+                background-color: #dd3333;
+                width: 30px;
+                height: 30px;
+                display: inline-block;
+                border: 1px #000 solid;
+            }
+
+            .under {
+                width: 30px;
+                height: 30px;
+                display: inline-block;
+                background-color: #00FF00;
+                border: 1px #000 solid;
+            }
+        </style>
+    </head>
+    <body>
+        <form>
+            <div>
+                <button type="submit" value="Submit" id="submit">Submit</button>
+              <button value="Clear" id="clear">Clear</button>
+            </div>
+        </form>
+
+        <div id="loom-container">
+            <div id="loom-div">
+            </div>
+        </div>
+    </body>
+  
+    <script src="script.js"></script>
+
+</html>
